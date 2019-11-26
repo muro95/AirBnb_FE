448df5ced30d04e833ddeffa5f9736f34a87c34a
phamngochau1506@gmail.com <haupham156>
2019-11-26 21:23:24 +0700

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,10 @@ import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {RouterModule, Routes} from '@angular/router';
-<<<<<<< HEAD
 import {AppComponent} from './app.component';
-=======
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
->>>>>>> c07d937456d555392cf813fa13059b56ae932ca6
 
 const routes: Routes = [
   {
@@ -20,7 +17,6 @@ const routes: Routes = [
   },
 ];
 
-// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
