bcaf6feaa2469bca510b50a377aaacc7db8e0a5a
phamngochau1506@gmail.com <haupham156>
2019-12-12 16:10:48 +0700

    update booking page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {HomeDetailComponent} from './components/home-detail/home-detail.componen
 import {HomeListForGuestComponent} from './components/home-list-for-guest/home-list-for-guest.component';
 import {RegisterHostComponent} from './components/register-host/register-host.component';
 import {AddHouseComponent} from './components/add-house/add-house.component';
+import {BookingComponent} from './components/booking/booking.component';
 
 
 const routes: Routes = [
@@ -37,6 +38,10 @@ const routes: Routes = [
   {
     path: 'home/add-house',
     component: AddHouseComponent
+  },
+  {
+    path: 'booking',
+    component: BookingComponent
   }
 ];
 
